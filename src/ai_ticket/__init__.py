39838f385537cbaabfbb8ef4d1e78bc9ebfa209d
mike dupont <mike.dupont@introspector.local>
2023-09-24 14:39:46 -0400

    adding handler for names

---
+++ src/ai_ticket/__init__.py
@@ -0,0 +1,9 @@
+import re
+def find_name(text):
+    pattern = r'(```)?\s*{\s*"messages"\s*:\s*\[\s*\{\s*"role"\s*:\s*"system"\s*,\s*\"content"\s*:\s*"You\s+are\s+(?P<name>[^,]+),.*'
+    match = re.match(pattern, text)
+    if match:
+        extracted_name = match.group(2)
+        return extracted_name
+    else:
+        return None
--- src/ai_ticket/backends/pygithub.py
+++ src/ai_ticket/backends/pygithub.py
@@ -1,25 +1,20 @@
 import os
+import re
+
 from dotenv import load_dotenv
 from github import Github
 from github import Auth
+from ai_ticket import find_name
 
+# FIXME move this to a context object
 # Load environment variables from .env
 load_dotenv()
-
 git_hub_pat = os.getenv("GITHUB_PAT")
 git_hub_repo = os.getenv("GITHUB_REPO")
-
-print("PAT",git_hub_pat)
-
-# using an access token
 auth = Auth.Token(git_hub_pat)
-
 g = Github(auth=auth)
-
-# Then play with your Github objects:
-#for repo in g.get_user().get_repos():
-#    print(repo.name)
 repo = g.get_repo(git_hub_repo)
+
 
 def get_issues():
     issues = repo.get_issues()
@@ -27,12 +22,26 @@ def get_issues():
         yield issue
 
 def get_existing_ticket(event):
+    body = event.get("content")
+    print("DEBUG get ",event)
+    for issue in get_issues():
+        data = issue.body
+        print("DEBUG check ",data)
+        name = find_name(data)
+        if name:
+            return issue
+
     return None
 
 def create_new_ticket(event):
+    raise Exception(str(event))
     # repo = g.get_repo("PyGithub/PyGithub")
     body = event.get("content")
     title = "Auto issue"
+    try:
+        body = "```" + json.dumps(json.loads(body),indent=2)  + "```"
+    except Exception as e:
+        print(e)
     return repo.create_issue(title=title, body=body)
     #Issue(title="This is a new issue", number=XXX)
     
