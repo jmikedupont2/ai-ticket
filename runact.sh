3e994822ed687ab55d0c1f3bf2d204e7c26412e4
mike dupont <mike.dupont@introspector.local>
2023-09-30 21:29:06 -0400

    working better
    now just need to add a job

--- runact.sh
+++ runact.sh
@@ -6,13 +6,9 @@
 
 # git clone https://github.com/moovweb/gvm
 # cd gvm/ &&./autogen.sh && make install
-
-act \
-    -s GITHUB_TOKEN=`cat github_pat.txt` \
-    -s GITHUB_PAT=`cat github_pat.txt` \
-    -s DOCKER=`cat dckr_pat_.txt` \
-    -P ubuntu-latest=localhost/my_local_act \
-    --verbose \
+# use .secrets PAT And DOCKER
+act    -P ubuntu-latest=localhost/my_local_act \
+       --verbose \
     --job python-package-build
 
 #    --pull=false \
