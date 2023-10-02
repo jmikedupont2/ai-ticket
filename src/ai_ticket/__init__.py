318dd265ce31365f8a571a7c5a19fcf3cea85f20
Mike DuPont <jmikedupont2@gmail.com>
2023-10-02 23:46:12 +0000

    bugfix

--- src/ai_ticket/__init__.py
+++ src/ai_ticket/__init__.py
@@ -1,6 +1,12 @@
 import re
+
+pattern = r'(```)?\s*{\s*"messages"\s*:\s*\[\s*\{\s*"role"\s*:\s*"system"\s*,\s*\"content"\s*:\s*"You\s+are\s+(?P<name>[^,]+),.*'
+
 def find_name(text):
-    pattern = r'(```)?\s*{\s*"messages"\s*:\s*\[\s*\{\s*"role"\s*:\s*"system"\s*,\s*\"content"\s*:\s*"You\s+are\s+(?P<name>[^,]+),.*'
+    if not text:
+        return False
+    if not isinstance(text,str):
+        return False
     match = re.match(pattern, text)
     if match:
         extracted_name = match.group(2)
