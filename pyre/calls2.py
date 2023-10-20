5a48d6d176d924bc2503f7caefd2a73b85ca3518
mike dupont <mike.dupont@introspector.local>
2023-10-20 16:39:23 -0400

    adding caller

---
+++ pyre/calls2.py
@@ -0,0 +1,23 @@
+from collections import Counter
+import pandas as pd
+data=[]
+import glob, json
+names = "./report/*/report/pyre_callgraph.json"
+#pyre_callgraph.json"
+
+files = {}
+calls=[]
+for name in glob.glob(names):
+  print(name)
+  with open(name) as fi :
+    for x in fi:
+      d = json.loads(x)
+      data = d["response"]
+      for x in list(data.keys()): # each function
+        if (len(data[x])):
+          for y in data[x]:
+            if "direct_target" in y:
+              calls.append( x +"-"+ y["direct_target"])
+df= Counter(calls)
+df2=pd.DataFrame(df.most_common(), columns=["name","count"])
+df2.to_csv("function_calls2.csv")
